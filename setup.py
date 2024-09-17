7908d4f0294452c47cf65cca1d133636b2f23c47
Daniel Lumian <dlumian@gmail.com>
2024-09-17 15:32:51 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='datascifuncs',
-    version='0.2.0',
+    version='0.3.0',
     author='Danny Lumian',
     author_email='dlumian@gmail.com',
     description='A package for loading/saving data and verifying paths.',
